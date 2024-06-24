7624a91c4a9e8d04f5660052459406d8c46907a8
Sawda-Hassan <Milgomaariin@gmail.com>
2024-06-24 07:15:11 +0300

    navbar-part

--- Food-delivery-app9/src/App.jsx
+++ Food-delivery-app9/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import Navbar from './Components/navbar/Navbar'
+import { assets } from './assets/assets'
 function App() {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Navbar/>
+      <u className='navbar-menu'> 
+        <li>home</li>
+        <li>menu</li>
+        <li>mobile-app</li>
+        <li>Contact us</li>
+
+      </u>
+      <div className='navbar-light'>
+        <img src={assets.search_icon} alt=''/>
+        <div className='dot'></div>
+        <button>sign in</button>
+      </div>
+
+    </div>
   )
 }
 
--- Food-delivery-app9/src/Components/navbar/Navbar.jsx
+++ Food-delivery-app9/src/Components/navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import './Navbar.css'
+import { assets } from '../../assets/assets'
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <img src={assets.logo}></img>
+        
+    </div>
   )
 }
 
