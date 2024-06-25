93dee3e6a26b0c03075e6c7ec407c2fe33f531f6
Sawda-Hassan <Milgomaariin@gmail.com>
2024-06-25 06:19:09 +0300

    navbar-complited

--- Food-delivery-app9/src/Components/navbar/Navbar.jsx
+++ Food-delivery-app9/src/Components/navbar/Navbar.jsx
@@ -1,29 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
-import k from 'Components/navbar/food-delivery (2)-01.png'
 
-import logo from 'Components/food-delivery (2)-01.png'
 function Navbar() {
+    const [menu,setmenu]=useState("menu")
   return (
     <div className='navbar'>
-        <img src={k} />
-        <u className='navbar-menu'> 
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile-app</li>
-        <li>Contact us</li>
+        <img src={assets.logo} />
+        <ul   className='navbar-menu'> 
+        <li onClick={()=>setmenu("home")}className={menu==="home"?"active":""}>home</li>
+        <li onClick={()=>setmenu("menu")}className={menu==="menu"?"active":""}>menu</li>
+        <li onClick={()=>setmenu("mobile-apps")}className={menu==="mobile-apps" ?"active":""}>mobile-app</li>
+        <li onClick={()=>setmenu("contact-us")}className={menu==="contact-us" ?"active":""}>Contact us</li>
 
-      </u>
+
+      </ul>
       <div className='navbar-light'>
         <img src={assets.search_icon} alt=''/>
-       < div className='navbar-search-icon'/>
+       <div className='navbar-search-icon'/>
         <img src={assets.basket_icon} alt=''/>
         <div className='dot'></div>
+        </div>
         <button>sign in</button>
       </div>
-        
-    </div>
+      
+    
   )
 }
 
