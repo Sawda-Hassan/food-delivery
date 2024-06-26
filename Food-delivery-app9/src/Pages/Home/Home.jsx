817212a0fd7027410857bedc1bb33509aa1e4e4b
Sawda-Hassan <Milgomaariin@gmail.com>
2024-06-26 05:59:27 +0300

    explore-menu-list

--- Food-delivery-app9/src/Pages/Home/Home.jsx
+++ Food-delivery-app9/src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
 import './Home.css'
+import ExploreMenu from '../../Components/Explore-menu/ExploreMenu'
 
 function Home() {
   return (
     <div>
       <Header/>
+      <ExploreMenu/>
     </div>
   )
 }
