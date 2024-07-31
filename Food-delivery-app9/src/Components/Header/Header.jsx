89371dc6c0dfb5c9d94090fad5e0899b768746aa
Sawda-Hassan <Milgomaariin@gmail.com>
2024-07-31 08:07:36 +0300

    ReadMe-file -section

--- Food-delivery-app9/src/Components/Header/Header.jsx
+++ Food-delivery-app9/src/Components/Header/Header.jsx
@@ -7,6 +7,7 @@ function Header() {
    <div className='header-content'>
    <h2>Order your favourite Food here</h2>
    <p>Choose from diverse menu faeturing adelectable array of dishes crafted </p>
+   
    <button>View menu</button>
    </div>
    </div>
